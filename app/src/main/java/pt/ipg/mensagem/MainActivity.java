80585fdf3c1c085fd85f756f8bcc89d551ca4565
Henrique Moreira <1700714@sal.ipg.pt>
2020-04-15 10:08:54 +0100

    Adicionadas validações para todos os campos (validações básicas).

--- app/src/main/java/pt/ipg/mensagem/MainActivity.java
+++ app/src/main/java/pt/ipg/mensagem/MainActivity.java
@@ -27,26 +27,55 @@ public class MainActivity extends AppCompatActivity {
         //verificação de dados para o nome
 
         if(nome.length() == 0){
-            editTextNome.setError("Falta preencher o nome");
+            editTextNome.setError("Falta preencher o nome.");
+            editTextNome.requestFocus();
             return;
         }
 
         EditText editTextTelemovel = (EditText) findViewById(R.id.editTextTelemovel);
         String telemovel = editTextTelemovel.getText().toString();
 
+        if(telemovel.length() < 9){
+            editTextTelemovel.setError("Telefone Inválido.Falta preencher o numero de telemóvel.");
+            editTextTelemovel.requestFocus();
+            return;
+        }
+
         EditText editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         String email = editTextEmail.getText().toString();
+
+        if(email.length() == 0){
+            editTextEmail.setError("Falta preencher o email.");
+            editTextEmail.requestFocus();
+            return;
+        }
 
         EditText editTextIdade = (EditText) findViewById(R.id.editTextIdade);
         String idade = editTextIdade.getText().toString();
 
+        if(idade.length() == 0){
+            editTextIdade.setError("Falta preencher a idade.");
+            editTextIdade.requestFocus();
+            return;
+        }
+
         EditText editTextPeso = (EditText) findViewById(R.id.editTextPeso);
         String peso = editTextPeso.getText().toString();
+
+        if(peso.length() == 0){
+            editTextPeso.setError("Falta preencher o peso.");
+            editTextPeso.requestFocus();
+            return;
+        }
 
         EditText editTextAltura = (EditText) findViewById(R.id.editTextAltura);
         String altura = editTextAltura.getText().toString();
 
-
+        if(altura.length() == 0){
+            editTextAltura.setError("Falta preencher a altura.");
+            editTextAltura.requestFocus();
+            return;
+        }
 
         intent.putExtra(App.NOME, nome);
         intent.putExtra(App.TELEMOVEL, telemovel);
